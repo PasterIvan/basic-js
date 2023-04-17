955ff283df16bc10be26248f4153dbaab8b5443b
PasterIvan <paster-ivan@yandex.by>
2023-04-17 18:14:22 +0300

    feat: task "Mine sweeper"

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,9 +21,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  return matrix.map((arr, x) => arr.map((el, y) => {
+    let res = 0;
+    for (let row = x - 1 >= 0 ? x - 1 : x; row < x + 2 && row < matrix.length; row++) {
+      for (let column = y - 1 >= 0 ? y - 1 : y; column < y + 2 && column < arr.length; column++) {
+        if (matrix[row][column] === true && !(row === x && column === y)) res += 1;
+      }
+    }
+    return res;
+  }))
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -26,52 +26,49 @@ class VigenereCipheringMachine {
   }
 
   encrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  //   if (!message || !key) {
-  //     throw new Error('Incorrect arguments!')
-  //   }
-  //   let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  //   let obj = {}
-  //   let messUp = message.toUpperCase()
-  //   let keyUp = key.toUpperCase()
-  //   for (let a = 0; a < alf.length; a++) {
-  //     obj[alf[a]] = a
-  //   }
-  //   let res = ''
-  //   for (let i = 0; i < messUp.length; i++) {
-  //     if (obj[messUp[i]]) {
-  //       res += alf[(obj[messUp[i]] + obj[keyUp[i % keyUp.length]]) % 26]
-  //     } else {
-  //       res += messUp[i]
-  //     }
-  //   }
-  //   return this.type ? res : res.split('').reverse().join('');
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let obj = {}
+    let messUp = message.toUpperCase()
+    let keyUp = key.toUpperCase()
+    for (let a = 0; a < alf.length; a++) {
+      obj[alf[a]] = a
+    }
+    let res = ''
+    for (let i = 0; i < messUp.length; i++) {
+      if (obj[messUp[i]]) {
+        res += alf[(obj[messUp[i]] + obj[keyUp[i % keyUp.length]]) % 26]
+      } else {
+        res += messUp[i]
+      }
+    }
+    return this.type ? res : res.split('').reverse().join('');
   }
   decrypt(encryptedMessage, key) {
-      throw new NotImplementedError('Not implemented');
-      // remove line with error and write your code here
-    // if (!encryptedMessage || !key) {
-    //   throw new Error('Incorrect arguments!')
-    // }
-    // let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    // let obj = {}
-    // let messUp = encryptedMessage.toUpperCase()
-    // let keyUp = key.toUpperCase()
-    //
-    // for (let a = 0; a < alf.length; a++) {
-    //   obj[alf[a]] = a
-    // }
-    // let res = ''
-    // for (let i = 0; i < messUp.length; i++) {
-    //   if (obj[messUp[i]]) {
-    //     res += alf[(obj[messUp[i]] - obj[keyUp[i % keyUp.length]] + 26) % 26]
-    //   } else {
-    //     res += messUp[i]
-    //   }
-    // }
-    //
-    // return this.type ? res : res.split('').reverse().join('');
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    let alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let obj = {}
+    let messUp = encryptedMessage.toUpperCase()
+    let keyUp = key.toUpperCase()
+
+    for (let a = 0; a < alf.length; a++) {
+      obj[alf[a]] = a
+    }
+    let res = ''
+    for (let i = 0; i < messUp.length; i++) {
+      if (obj[messUp[i]]) {
+        res += alf[(obj[messUp[i]] - obj[keyUp[i % keyUp.length]] + 26) % 26]
+      } else {
+        res += messUp[i]
+      }
+    }
+
+    return this.type ? res : res.split('').reverse().join('');
   }
 }
 
